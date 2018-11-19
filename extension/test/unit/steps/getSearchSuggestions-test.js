edacd0079d18bccf468cced4520e44a8d7a38674
Pascal Vomhoff <Pascal.Vomhoff@shopgate.com>
2018-11-19 23:04:37 +0100

    Fix tests

--- extension/test/unit/steps/getSearchSuggestions-test.js
+++ extension/test/unit/steps/getSearchSuggestions-test.js
@@ -1,7 +1,7 @@
 const step = require('../../../steps/getSearchSuggestions')
 const assert = require('assert')
 const nock = require('nock')
-const request = require('request')
+const request = require('request-promise-native')
 
 describe('getSearchSuggestions', () => {
   let context, input
@@ -28,19 +28,19 @@ describe('getSearchSuggestions', () => {
     nock.enableNetConnect()
   })
 
-  it('should build a request and parse the response', (done) => {
+  it('should build a request and parse the response', async () => {
     const hits = [
-      {id: '1', highlights: {name: '', child_names: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk'}},
-      {id: '2', highlights: {name: 'some$start$,high$end$foofoofoo something like that$next$foooLuLuLuLu', child_names: ''}}, // , should be removed
-      {id: '3', highlights: {name: 'some$start$+high$end$foofoofoo+ something like  that$next$foooLuLuLuLu', child_names: ''}}, // + should be removed
-      {id: '4', highlights: {name: 'some$start$high$end$fooFoofoo something like that$next$foooLuLuLuLu', child_names: ''}}, // same but different case
-      {id: '5', highlights: {name: 'some$start$high$end$fooFoofoo something like that$next$foooLuLuLuLu', child_names: ''}}, // 3 times, so this is the correct case
-      {id: '6', highlights: {name: 'some$start$high$end$fooFoofoo something like that$next$foooLuLuLuLu', child_names: ''}}, // should be sorted to first position
-      {id: '7', highlights: {name: '', child_names: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk'}},
-      {id: '8', highlights: {name: '', child_names: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk'}},
-      {id: '9', highlights: {name: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk', child_names: ''}},
-      {id: 'a', highlights: {name: 'foooo$start$-hithithithit-$end$', child_names: ''}}, // only match[1], hyphen at end should be removed
-      {id: 'b', highlights: {name: 'foooo$start$hithit$end$', child_names: 'foooo$start$hithit$end$'}} // hit to short, should be removed
+      { id: '1', highlights: { name: '', child_names: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk' } },
+      { id: '2', highlights: { name: 'some$start$,high$end$foofoofoo something like that$next$foooLuLuLuLu', child_names: '' } }, // , should be removed
+      { id: '3', highlights: { name: 'some$start$+high$end$foofoofoo+ something like  that$next$foooLuLuLuLu', child_names: '' } }, // + should be removed
+      { id: '4', highlights: { name: 'some$start$high$end$fooFoofoo something like that$next$foooLuLuLuLu', child_names: '' } }, // same but different case
+      { id: '5', highlights: { name: 'some$start$high$end$fooFoofoo something like that$next$foooLuLuLuLu', child_names: '' } }, // 3 times, so this is the correct case
+      { id: '6', highlights: { name: 'some$start$high$end$fooFoofoo something like that$next$foooLuLuLuLu', child_names: '' } }, // should be sorted to first position
+      { id: '7', highlights: { name: '', child_names: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk' } },
+      { id: '8', highlights: { name: '', child_names: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk' } },
+      { id: '9', highlights: { name: 'fooooooooooooooo$next$ barararar$start$xxxx$end$mmmmmmmm kkkkkk', child_names: '' } },
+      { id: 'a', highlights: { name: 'foooo$start$-hithithithit-$end$', child_names: '' } }, // only match[1], hyphen at end should be removed
+      { id: 'b', highlights: { name: 'foooo$start$hithit$end$', child_names: 'foooo$start$hithit$end$' } } // hit to short, should be removed
     ]
     const api = nock('http://cloudsearch.de')
       .get('/search')
@@ -57,100 +57,84 @@ describe('getSearchSuggestions', () => {
         })
         return true
       })
-      .reply(200, {hits: {hit: hits}})
+      .reply(200, { hits: { hit: hits } })
 
-    step(context, input, (err, result) => {
-      assert.ifError(err)
-      api.done()
+    const result = await step(context, input)
+    api.done()
 
-      assert.deepEqual(result, {suggestions: [
-        'highfooFoofoo',
-        'highfooFoofoo something',
-        'highfooFoofoo something like',
-        'xxxxmmmmmmmm',
-        'xxxxmmmmmmmm kkkkkk',
-        '-hithithithit'
-      ]})
-      done()
-    })
+    assert.deepEqual(result, { suggestions: [
+      'highfooFoofoo',
+      'highfooFoofoo something',
+      'highfooFoofoo something like',
+      'xxxxmmmmmmmm',
+      'xxxxmmmmmmmm kkkkkk',
+      '-hithithithit'
+    ] })
   })
 
-  it('should return an empty array if there where no hits', (done) => {
+  it('should return an empty array if there where no hits', async () => {
     const api = nock('http://cloudsearch.de')
       .get('/search')
       .query(() => true)
-      .reply(200, {hits: {hit: []}})
+      .reply(200, { hits: { hit: [] } })
 
-    step(context, input, (err, result) => {
-      api.done()
-      assert.ifError(err)
-      assert.deepEqual(result, {suggestions: []})
-      done()
-    })
+    const result = await step(context, input)
+    api.done()
+    assert.deepEqual(result, { suggestions: [] })
   })
 
-  it('should return an empty array if there where no hit array', (done) => {
+  it('should return an empty array if there where no hit array', async () => {
     const api = nock('http://cloudsearch.de')
       .get('/search')
       .query(() => true)
-      .reply(200, {hits: {}})
+      .reply(200, { hits: {} })
 
-    step(context, input, (err, result) => {
-      api.done()
-      assert.ifError(err)
-      assert.deepEqual(result, {suggestions: []})
-      done()
-    })
+    const result = await step(context, input)
+    api.done()
+    assert.deepEqual(result, { suggestions: [] })
   })
 
-  it('should return an empty array if there where no hits object', (done) => {
+  it('should return an empty array if there where no hits object', async () => {
     const api = nock('http://cloudsearch.de')
       .get('/search')
       .query(() => true)
       .reply(200, {})
 
-    step(context, input, (err, result) => {
-      api.done()
-      assert.ifError(err)
-      assert.deepEqual(result, {suggestions: []})
-      done()
-    })
+    const result = await step(context, input)
+    api.done()
+    assert.deepEqual(result, { suggestions: [] })
   })
 
-  it('should return an empty array if there where no json aswer', (done) => {
+  it('should return an empty array if there where no json aswer', async () => {
     const api = nock('http://cloudsearch.de')
       .get('/search')
       .query(() => true)
       .reply(200, 'fooo{')
 
-    step(context, input, (err, result) => {
-      api.done()
-      assert.ifError(err)
-      assert.deepEqual(result, {suggestions: []})
-      done()
-    })
+    const result = await step(context, input)
+    api.done()
+    assert.deepEqual(result, { suggestions: [] })
   })
 
-  it('should throw an error if statusCode is != 200 and return rawBody if its no valid json', (done) => {
+  it('should throw an error if statusCode is != 200 and return rawBody if its no valid json', async () => {
     const api = nock('http://cloudsearch.de')
       .get('/search')
       .query(() => true)
       .reply(500, 'something{')
 
-    step(context, input, (err) => {
+    try {
+      await step(context, input)
+      assert.fail()
+    } catch (err) {
+      assert.equal(err.message, '500 - "something{"')
+    } finally {
       api.done()
-      assert.ok(err)
-      assert.equal(err.message, 'something{')
-      done()
-    })
+    }
   })
 
-  it('should return an empty array if searchPhrase.length < 2', (done) => {
+  it('should return an empty array if searchPhrase.length < 2', async () => {
     input.searchPhrase = 'a'
-    step(context, input, (err, res) => {
-      assert.ifError(err)
-      assert.deepEqual(res, {suggestions: []})
-      done()
-    })
+    const res = await step(context, input)
+    assert.deepEqual(res, { suggestions: [] })
   })
 })
